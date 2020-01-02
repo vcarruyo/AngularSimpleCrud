2652d2584356de910663766a2c9121418d491489
victoriano <victorianocarruyo@gmail.com>
2020-01-02 04:57:30 -0300

    Firs Commit for Angular Simple Crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './moderls/employee'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+  
+  employeeArray: Employee [] = [
+    {id: 1, name: "ada", country: "bosnia"},
+    {id: 2, name: "alan", country: "nigeria"},
+    {id: 3, name: "amanda", country: "sambia"}
+  ]
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id=this.employeeArray.length+1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee:Employee){
+    this.selectedEmployee=employee;
+  }
+
+  delete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
